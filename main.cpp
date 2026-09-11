4565bc807a405c5d2c0688f7aea0a6c8a647a3db
Gabriel Ford <gabriel@gford.dev>
2026-09-11 19:51:53 +0000

    Add proper affinity pinning

--- main.cpp
+++ main.cpp
@@ -1,16 +1,39 @@
+#include <ostream>
 #include <cstdio>
 #include <cstdlib>
-#include <thread>
+#include <ctime>
+#include <iostream>
 #include <vector>
+#include <chrono>
+#include <thread>
+#include <ctime>
 #include <sched.h>
 #include <pthread.h>
 #include <unistd.h>
 
-void* worker (void *) {
-  return 0;
+void* worker (void * runtime) {
+  unsigned long counter = 0;
+  time_t start = time(nullptr);
+  time_t current = time(nullptr);
+  while (difftime(current, start) < *(long *) runtime) {
+    counter++;
+    std::this_thread::sleep_for(std::chrono::milliseconds(1));
+    time(&current);
+  }
+  return NULL;
 }
 
-int main() {
+int main(int argc, char* argv[]) {
+  if (argc == 1 || argc > 2) {
+    std::cerr << "Correct syntax: benign [time in seconds]" << std::endl;
+    exit(-1);
+  }
+
+  char* pEnd;
+  long runtime = strtol(argv[1], &pEnd, 10);
+
+  std::cout << "Starting a run for " << runtime << " seconds" << std::endl;
+
   cpu_set_t cpuset;
   CPU_ZERO(&cpuset);
 
@@ -19,13 +42,34 @@ int main() {
     exit(-1);
   }
 
-  std::vector<pthread_t> thread_ids;
   int num_cores = sysconf(_SC_NPROCESSORS_ONLN);
 
+  // Get active cores by reading through the mask
+  std::vector<int> active_cores;
   for (size_t i = 0; i < num_cores; i++) {
-    pthread_t worker_thread;
-    thread_ids.push_back(worker_thread);
-    pthread_create(&thread_ids[i], NULL, worker, nullptr);
-    pthread_setaffinity_np(worker_thread, sizeof(cpu_set_t), &cpuset);
+    if (CPU_ISSET(i, &cpuset)) {
+      active_cores.push_back(i);
+    }
   }
+
+  std::vector<pthread_t> thread_ids(active_cores.size());
+
+  for (size_t i = 0; i < active_cores.size(); i++) {
+    pthread_t worker_thread;
+    cpu_set_t mask;
+    CPU_ZERO(&mask);
+    CPU_SET(active_cores[i], &mask);
+
+    pthread_attr_t attr;
+    pthread_attr_init(&attr);
+    pthread_attr_setaffinity_np(&attr, sizeof(cpu_set_t), &mask);
+    pthread_create(&thread_ids[i], &attr, worker, &runtime);
+    pthread_attr_destroy(&attr);
+  }
+
+  for (auto& thread : thread_ids) {
+    pthread_join(thread, nullptr);
+  }
+
+  std::cout << "Run complete!" << std::endl;
 }
